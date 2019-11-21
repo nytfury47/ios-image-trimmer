2ea21e0d7b5bc4f42bbbf24e82f55eab76a16ea1
Gerardo Carlos Roderico Tan <gerardocarlosroderico.tan@qvcjp.com>
2019-11-21 12:18:37 +0900

    setup project

---
+++ ImageTrimmer/ImageTrimmer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageTrimmer
+//
+//  Created by Gerardo Carlos Roderico Tan on 2019/11/21.
+//  Copyright © 2019 nytfury47. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
