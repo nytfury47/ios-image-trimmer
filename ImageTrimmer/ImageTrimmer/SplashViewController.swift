ae21c9bf7d4121bcc8df30fb99677031c771de24
Gerardo Carlos Roderico Tan <gerardocarlosroderico.tan@qvcjp.com>
2019-11-21 18:08:21 +0900

    fix splash vc

--- ImageTrimmer/ImageTrimmer/SplashViewController.swift
+++ ImageTrimmer/ImageTrimmer/SplashViewController.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class SplashViewController: UIViewController {
 
+    @IBOutlet weak var lblAppName: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+//        lblAppName.font = lblAppName.font.withSize(getScaledHeight(base: BASE_FONT_SIZE_S))
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
 
+    override func viewDidAppear(_ animated: Bool) {
+        UIView.transition(with: self.lblAppName, duration: 1, options: .transitionCrossDissolve, animations: {
+            self.lblAppName.isHidden = true
+            
+            DispatchQueue.main.asyncAfter(deadline:.now() + 1, execute: {
+                self.performSegue(withIdentifier: "segueToMainView", sender: nil)
+            })
+        })
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
 }
 
